31230f364b1f3d45041d6bab5207e36b10dd7d7b
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-04-13 03:51:25 +0000

    Tạo project. DesignCanvas hiện tại chỉ hoạt động với 1 control được chọn. Chưa hổ trợ chọn nhiều control. Sự kiện right click mới cài đặt thử. Chưa có gì hết.

---
+++ trunk/MashupDesignTool/MashupDesignTool/DesignCanvas.xaml.cs
@@ -0,0 +1,472 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.Xml;
+using SharpGIS.MouseExtensions;
+
+namespace MashupDesignTool
+{
+    public partial class DesignCanvas : UserControl
+    {
+        #region enum resize direction
+        enum ResizeDirection
+        {
+            TOP,
+            LEFT,
+            RIGHT,
+            BOTTOM,
+            TOPLEFT,
+            TOPRIGHT,
+            BOTTOMLEFT,
+            BOTTOMRIGHT
+        }
+        #endregion enum resize direction
+
+        UserControl selectedControl;
+        ProxyControl selectedProxyControl;
+        ResizeDirection resizeDirection;
+        Point clickPoint;
+        Point beginResizedPoint;
+        bool isControlCaptured;
+        bool isResizing;
+        bool canResize;
+        bool isShowingContextMenu;
+        List<UserControl> controls;
+        List<ProxyControl> proxyControls;
+
+        public List<UserControl> Controls
+        {
+            get { return controls; }
+            set { controls = value; }
+        }
+
+        public DesignCanvas()
+        {
+            InitializeComponent();
+
+            selectedControl = null;
+            isResizing = false;
+            canResize = false;
+            isShowingContextMenu = false;
+            
+            CursorManager.InitCursor(LayoutRoot);
+
+            proxyControls = new List<ProxyControl>();
+            controls = new List<UserControl>();
+
+            this.AttachRightClick(OnRightClick);
+        }
+
+        public void AddControl(UserControl uc, double x, double y, int width, int height)
+        {
+            uc.Margin = new Thickness(0, 0, 0, 0);
+            ProxyControl pc = new ProxyControl(uc, x, y, width, height);
+            proxyControls.Add(pc);
+            controls.Add(uc);
+            pc.UpdateVisibility(System.Windows.Visibility.Collapsed);
+
+            LayoutRoot.Children.Add(uc);
+            LayoutRoot.Children.Add(pc);
+
+            pc.MouseLeftButtonDown += new MouseButtonEventHandler(control_MouseLeftButtonDown);
+            pc.MouseMove += new MouseEventHandler(control_MouseMove);
+            pc.MouseLeftButtonUp += new MouseButtonEventHandler(control_MouseLeftButtonUp);
+        }
+
+        #region dragdrop control
+        void control_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            ProxyControl pc = (ProxyControl)sender;
+            pc.ReleaseMouseCapture();
+            isControlCaptured = false;
+        }
+
+        void control_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (isControlCaptured)
+            {
+                Point pt = e.GetPosition(LayoutRoot);
+                double x = pt.X - clickPoint.X;
+                double y = pt.Y - clickPoint.Y;
+                ProxyControl pc = (ProxyControl)sender;
+                UserControl uc = pc.RealControl;
+
+                if (x < 0 || y < 0 || x + uc.Width >= this.ActualWidth || y + uc.Height >= this.ActualHeight)
+                {
+                    if (x < 0)
+                        x = 0;
+                    else if (x + uc.Width >= this.ActualWidth)
+                        x = this.ActualWidth - uc.Width - 1;
+
+                    if (y < 0)
+                        y = 0;
+                    else if (y + uc.Height >= this.ActualHeight)
+                        y = this.ActualHeight - uc.Height - 1;
+                }
+                pc.MoveControl(x, y);
+            }
+        }
+
+        void control_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!canResize)
+            {
+                if (selectedProxyControl != null)
+                    selectedProxyControl.UpdateVisibility(System.Windows.Visibility.Collapsed);
+
+                ProxyControl pc = (ProxyControl)sender;
+                clickPoint = e.GetPosition(pc.RealControl);
+                pc.CaptureMouse();
+                isControlCaptured = true;
+
+                pc.UpdateVisibility(System.Windows.Visibility.Visible);
+                selectedControl = pc.RealControl;
+                selectedProxyControl = pc;
+            }
+
+            if (isShowingContextMenu)
+            {
+                HideContextMenu();
+            }
+        }
+        #endregion dragdrop control
+
+        #region resize control
+        private void LayoutRoot_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedControl != null)
+            {
+                if (!isResizing)
+                {
+                    CheckCanResize(e);
+                }
+                else
+                {
+                    OnResizing(e);
+                }
+            }
+        }
+
+        private void LayoutRoot_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (canResize)
+            {
+                isResizing = true;
+            }
+            else if (!isControlCaptured)
+            {
+                selectedControl = null;
+                if (selectedProxyControl != null)
+                    selectedProxyControl.UpdateVisibility(System.Windows.Visibility.Collapsed);
+            }
+            
+            if (isShowingContextMenu)
+            {
+                HideContextMenu();
+            }
+        }
+
+        private void LayoutRoot_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (isResizing)
+            {
+                Point pt = e.GetPosition(LayoutRoot);
+                ResizeControl(pt);
+                isResizing = false;
+            }
+        }
+
+        private void CheckCanResize(MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(selectedControl);
+            canResize = false;
+
+            #region marked mouse position 
+            int hDirection = 0;
+            int vDirection = 0;
+
+            if (pt.X >= -8 && pt.X <= 0)
+                hDirection = 1;
+            else if (pt.X > 0 && pt.X <= 4)
+                hDirection = 2;
+            else if (pt.X > 4 && pt.X < selectedControl.Width - 4)
+                hDirection = 3;
+            else if (pt.X >= selectedControl.Width - 4 && pt.X < selectedControl.Width)
+                hDirection = 4;
+            else if (pt.X >= selectedControl.Width && pt.X <= selectedControl.Width + 8)
+                hDirection = 5;
+
+            if (pt.Y >= -8 && pt.Y <= 0)
+                vDirection = 1;
+            else if (pt.Y > 0 && pt.Y <= 4)
+                vDirection = 2;
+            else if (pt.Y > 4 && pt.Y < selectedControl.Height - 4)
+                vDirection = 3;
+            else if (pt.Y >= selectedControl.Height - 4 && pt.Y <= selectedControl.Height)
+                vDirection = 4;
+            else if (pt.Y >= selectedControl.Height && pt.Y <= selectedControl.Height + 8)
+                vDirection = 5;
+            #endregion marked mouse position
+
+            #region set cursor, begin resized point, resize direction
+            beginResizedPoint.X = beginResizedPoint.Y = 0;
+            if (hDirection == 1)
+            {
+                beginResizedPoint.X = pt.X;
+                if (vDirection == 1 || vDirection == 2)
+                {
+                    resizeDirection = ResizeDirection.TOPLEFT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWNES);
+                }
+                else if (vDirection == 3)
+                {
+                    resizeDirection = ResizeDirection.LEFT;
+                    canResize = true;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWE);
+                }
+                else if (vDirection == 4 || vDirection == 5)
+                {
+                    resizeDirection = ResizeDirection.BOTTOMLEFT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y - selectedControl.Height;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeENWS);
+                }
+            }
+            else if (hDirection == 2)
+            {
+                beginResizedPoint.X = pt.X;
+                if (vDirection == 1)
+                {
+                    resizeDirection = ResizeDirection.TOPLEFT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWNES);
+                }
+                else if (vDirection == 5)
+                {
+                    resizeDirection = ResizeDirection.BOTTOMLEFT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y - selectedControl.Height;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeENWS);
+                }
+            }
+            else if (hDirection == 3)
+            {
+                if (vDirection == 1)
+                {
+                    resizeDirection = ResizeDirection.TOP;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeNS);
+                }
+                else if (vDirection == 5)
+                {
+                    this.Cursor = Cursors.SizeNS;
+                    resizeDirection = ResizeDirection.BOTTOM;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y - selectedControl.Height;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeNS);
+                }
+            }
+            else if (hDirection == 4)
+            {
+                beginResizedPoint.X = pt.X - selectedControl.Width;
+                if (vDirection == 1)
+                {
+                    resizeDirection = ResizeDirection.TOPRIGHT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeENWS);
+                }
+                else if (vDirection == 5)
+                {
+                    resizeDirection = ResizeDirection.BOTTOMRIGHT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y - selectedControl.Height;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWNES);
+                }
+            }
+            else if (hDirection == 5)
+            {
+                beginResizedPoint.X = pt.X - selectedControl.Width;
+                if (vDirection == 1 || vDirection == 2)
+                {
+                    resizeDirection = ResizeDirection.TOPRIGHT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeENWS);
+                }
+                else if (vDirection == 3)
+                {
+                    this.Cursor = Cursors.SizeWE;
+                    resizeDirection = ResizeDirection.RIGHT;
+                    canResize = true;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWE);
+                }
+                else if (vDirection == 4 || vDirection == 5)
+                {
+                    resizeDirection = ResizeDirection.BOTTOMRIGHT;
+                    canResize = true;
+                    beginResizedPoint.Y = pt.Y - selectedControl.Height;
+                    CursorManager.ChangeCursor(this, CursorManager.CursorType.SizeWNES);
+                }
+            }
+            #endregion set cursor, begin resized point, resize direction
+
+            if (!canResize)
+                CursorManager.ChangeCursor(this, CursorManager.CursorType.Arrow);
+
+            CursorManager.UpdateCursorPosition(e.GetPosition(LayoutRoot));
+        }
+
+        private void OnResizing(MouseEventArgs e)
+        {
+            Point pt = e.GetPosition(LayoutRoot);
+            bool b = false;
+            if (pt.X < beginResizedPoint.X)
+            {
+                b = true;
+                pt.X = 0;
+            }
+            else if (pt.X >= this.ActualWidth + beginResizedPoint.X)
+            {
+                b = true;
+                pt.X = this.ActualWidth - 1;
+            }
+            if (pt.Y < beginResizedPoint.Y)
+            {
+                b = true;
+                pt.Y = 0;
+            }
+            else if (pt.Y >= this.ActualHeight + beginResizedPoint.Y)
+            {
+                b = true;
+                pt.Y = this.ActualHeight - 1;
+            }
+
+            ResizeControl(pt);
+
+            if (b == true)
+            {
+                isResizing = false;
+                canResize = false;
+                CursorManager.ChangeCursor(this, CursorManager.CursorType.Arrow);
+            }
+        }
+
+        private void ResizeControl(Point pt)
+        {
+            double x, y;
+            double width, height;
+
+            x = (double)selectedControl.GetValue(Canvas.LeftProperty);
+            y = (double)selectedControl.GetValue(Canvas.TopProperty);
+            width = selectedControl.Width;
+            height = selectedControl.Height;
+
+            #region calculate new position, dimension
+            if (resizeDirection == ResizeDirection.TOP || resizeDirection == ResizeDirection.TOPLEFT || resizeDirection == ResizeDirection.TOPRIGHT)
+            {
+                double h = height;
+                height = height - (pt.Y - y - beginResizedPoint.Y);
+                if (height < 0)
+                {
+                    height = 0;
+                    y = y + h;
+                }
+                else
+                    y = pt.Y - beginResizedPoint.Y;
+            }
+            if (resizeDirection == ResizeDirection.LEFT || resizeDirection == ResizeDirection.TOPLEFT || resizeDirection == ResizeDirection.BOTTOMLEFT)
+            {
+                double w = width;
+                width = width - (pt.X - x - beginResizedPoint.X);
+                if (width < 0)
+                {
+                    width = 0;
+                    x = x + w;
+                }
+                else
+                    x = pt.X - beginResizedPoint.X;
+            }
+            if (resizeDirection == ResizeDirection.BOTTOM || resizeDirection == ResizeDirection.BOTTOMLEFT || resizeDirection == ResizeDirection.BOTTOMRIGHT)
+            {
+                height = pt.Y - beginResizedPoint.Y - y;
+                height = height < 0 ? 0 : height;
+            }
+            if (resizeDirection == ResizeDirection.RIGHT || resizeDirection == ResizeDirection.TOPRIGHT || resizeDirection == ResizeDirection.BOTTOMRIGHT)
+            {
+                width = pt.X - beginResizedPoint.X - x;
+                width = width < 0 ? 0 : width;
+            }
+            #endregion calculate new position, dimension
+
+            selectedProxyControl.ResizeControl(width, height);
+            selectedProxyControl.MoveControl(x, y);
+            CursorManager.UpdateCursorPosition(pt);
+        }
+        #endregion resize control
+
+        private void LayoutRoot_MouseLeave(object sender, MouseEventArgs e)
+        {
+            isResizing = false;
+            CursorManager.ChangeCursor(this, CursorManager.CursorType.Arrow);
+        }
+
+        #region contextmenu
+        private void OnRightClick(object sender, SharpGIS.MouseExtensions.RightMouseButtonEventArgs args)
+        {
+            PositionContextMenu(args.GetPosition(contextMenu.Parent as UIElement), true);
+            args.Handled = true;
+            gridContextMenu.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void PositionContextMenu(Point p, bool useTransition)
+        {
+            if (useTransition)
+                contextMenu.IsOpen = false;
+            contextMenu.HorizontalOffset = p.X;
+            contextMenu.VerticalOffset = p.Y;
+            contextMenu.IsOpen = true;
+        }
+
+        private void contextMenu_Opened(object sender, EventArgs e)
+        {
+            isShowingContextMenu = true;
+            ShowPopup.Begin();
+        }
+
+        private void contextMenu_Closed(object sender, EventArgs e)
+        {
+            isShowingContextMenu = false;
+            HidePopup.Begin();
+        }
+
+        private void HideContextMenu()
+        {
+            HidePopup.Begin();
+            isShowingContextMenu = false;
+            gridContextMenu.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void lbContextMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            HideContextMenu();
+
+            lbContextMenu.SelectedIndex = -1;
+        }
+        #endregion contextmenu
+    }
+}
---
+++ trunk/MashupDesignTool/MashupDesignTool/DesignPage.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+using System.Xml;
+
+namespace MashupDesignTool
+{
+    public partial class MainPage : UserControl
+    {
+        Dictionary<String, Assembly> LoadedAssembly = new Dictionary<String, Assembly>();
+        String controlName = "";
+        String clientRoot = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            string absoluteUri = Application.Current.Host.Source.AbsoluteUri;
+            int lastSlash = absoluteUri.LastIndexOf("/");
+            clientRoot = absoluteUri.Substring(0, lastSlash + 1);
+
+            DownloadControlInfo();
+
+            designCanvas1.Height = (double)new System.Windows.LengthConverter().ConvertFromString("Auto");
+            designCanvas1.Width = (double)new System.Windows.LengthConverter().ConvertFromString("Auto");
+        }
+
+        #region download info.xml and contruct control tree
+        private void DownloadControlInfo()
+        {
+            //Get the Uri to ClientBin directory:
+            string absoluteUri = Application.Current.Host.Source.AbsoluteUri;
+            int lastSlash = absoluteUri.LastIndexOf("/");
+            string assemblyPath = absoluteUri.Substring(0, lastSlash + 1);
+            //And tack on name of assembly:
+            assemblyPath += "controls/info.xml";
+
+            Uri uri = new Uri(assemblyPath, UriKind.Absolute);
+            //Start an async download:
+            WebClient webClient = new WebClient();
+            webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(webClient_DownloadControlInfoCompleted);
+            webClient.OpenReadAsync(uri);
+        }
+
+        private void webClient_DownloadControlInfoCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            XmlReader reader = XmlReader.Create(e.Result);
+            reader.Read();
+            int number;
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "number")
+                    {
+                        number = int.Parse(reader.ReadInnerXml());
+                    }
+                    else if (reader.Name == "name")
+                    {
+                        AddControlToTree(reader.ReadInnerXml(), "test control");
+                    }
+                }
+            }
+        }
+
+        private void AddTreeViewItem(TreeView t, string itemName, object item)
+        {
+            foreach (TreeViewItem tv in t.Items)
+                if (itemName == tv.Header.ToString())
+                {
+                    tv.Items.Add(item);
+                    return;
+                }
+            TreeViewItem tvi = new TreeViewItem();
+            tvi.Header = itemName;
+            tvi.Items.Add(item);
+            t.Items.Add(tvi);
+        }
+
+        private void AddControlToTree(string name, string group)
+        {
+            StackPanel sp = new StackPanel();
+            sp.Orientation = Orientation.Horizontal;
+
+            String imagePath = clientRoot + "controls/" + name + ".png";
+
+            Image im = new Image();
+            im.Source = new BitmapImage(new Uri(imagePath));
+            Label lb = new Label();
+            lb.Content = name;
+            lb.Margin = new Thickness(lb.Margin.Left + 5, lb.Margin.Top, lb.Margin.Right, lb.Margin.Bottom);
+            sp.Children.Add(im);
+            sp.Children.Add(lb);
+
+            sp.MouseLeftButtonDown += new MouseButtonEventHandler(sp_MouseLeftButtonDown);
+            AddTreeViewItem(TreeControl, group, sp);
+        }
+        #endregion
+
+        private void DownloadControl()
+        {
+            String assemblyPath = clientRoot + "controls/" + controlName + ".dll";
+
+            Uri uri = new Uri(assemblyPath, UriKind.Absolute);
+            //Start an async download:
+            WebClient webClient = new WebClient();
+            webClient.OpenReadCompleted += new OpenReadCompletedEventHandler(webClient_DownloadControlCompleted);
+            webClient.OpenReadAsync(uri);
+        }
+
+        private void webClient_DownloadControlCompleted(object sender, OpenReadCompletedEventArgs e)
+        {
+            AssemblyPart assemblyPart = new AssemblyPart();
+            Assembly assembly = assemblyPart.Load(e.Result);
+            //UIElement o = assembly.CreateInstance("Plugin.Controls." + controlName) as UIElement;            
+            LoadedAssembly.Add(controlName, assembly);
+
+
+            ////////////////////////////////////////////////////////////////////////////////////
+            //test thoi
+            UserControl o = LoadedAssembly[controlName].CreateInstance("control." + controlName) as UserControl;
+            //UIElement o = LoadedAssembly[controlName].CreateInstance(controlName) as UIElement;
+            if (o == null)
+                return;
+            //spanel.Children[1] = o;
+            //InitControl(o, 500, 400, 100.0, 100.0);
+
+            designCanvas1.AddControl(o, 100.0, 100.0, 50, 40);
+
+            //string content = "";
+            //Type t = spanel.GetType();
+            //foreach (PropertyInfo pinfo in t.GetProperties())
+            //{
+            //    content += pinfo.ToString() + "\n";
+            //}
+            //textBlock1.Text = content;
+        }
+
+        //when user click a control, download it if needed.
+        private void sp_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            StackPanel sp = (StackPanel)sender;
+            Label lb = (Label)sp.Children[1];
+            controlName = lb.Content.ToString();
+            if (LoadedAssembly.Keys.Contains(controlName) == true)
+                return;
+            DownloadControl();
+        }
+    }
+}
