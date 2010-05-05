b2ea275d3bfe0eacd9e35899117002574a191b45
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-05 09:03:25 +0000

    Them project html rich text area de hien thi html. Cai dat lai control NewsReader. Them 1 so effect Project Testcontrol1 la de test cai NewsReader.

---
+++ trunk/MashupDesignTool/Effect/MagnifierOverEffect.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using System.Windows.Interactivity;
+
+namespace Effect
+{
+    public class MagnifierOverEffect
+    {
+        private static Dictionary<UIElement, MagnifierOverBehavior> behaviors = new Dictionary<UIElement, MagnifierOverBehavior>();
+
+        public static void AttachEffect(UIElement element, double magnification)
+        {
+            if (behaviors.ContainsKey(element))
+            {
+                MagnifierOverBehavior behavior = behaviors[element];
+                behavior.ChangeMagnification(magnification);
+            }
+
+            else
+            {
+                MagnifierOverBehavior behavior = new MagnifierOverBehavior();
+                behavior.ChangeMagnification(magnification);
+                Interaction.GetBehaviors(element).Add(behavior);
+                behaviors.Add(element, behavior);
+            }
+        }
+
+        public static bool DetachEffect(UIElement element)
+        {
+            if (!behaviors.ContainsKey(element))
+                return false;
+            Interaction.GetBehaviors(element).Remove(behaviors[element]);
+            behaviors.Remove(element);
+            return true;
+        }
+
+        public static bool ChangeMagnification(UIElement element, double magnification)
+        {
+            if (behaviors.ContainsKey(element))
+            {
+                MagnifierOverBehavior behavior = behaviors[element];
+                behavior.ChangeMagnification(magnification);
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- trunk/MashupDesignTool/NewsReaderControl/NewsPage.xaml.cs
+++ trunk/MashupDesignTool/NewsReaderControl/NewsPage.xaml.cs
@@ -9,26 +9,54 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
-using Divelements.SilverlightTools;
 
 namespace NewsReaderControl
 {
     public partial class NewsPage : UserControl
     {
+        public delegate void OnLoadPageComplete(NewsPage sender);
+        public event OnLoadPageComplete LoadPageComplete;
+
+        string url = "";
+
         public NewsPage()
         {
             InitializeComponent();
+            page.SetDefaultStyles();
         }
 
         public string PageURL
         {
-            get { return page.SourceUri.AbsoluteUri; }
-            set { page.SourceUri = new Uri(value); }
+            get { return url; }
+            set 
+            {
+                url = value;
+                ServiceReference1.MashupToolWCFServiceClient client = new ServiceReference1.MashupToolWCFServiceClient();
+                client.GetStringFromURLCompleted += new EventHandler<ServiceReference1.GetStringFromURLCompletedEventArgs>(client_GetStringFromURLCompleted);
+                client.GetStringFromURLAsync(url);
+            }
         }
 
-        public void SetPageContent(string content)
+        public void LoadHTML(string html)
         {
-            page.SourceHtml = content;
+            page.Load(html);
+        }
+
+        void client_GetStringFromURLCompleted(object sender, ServiceReference1.GetStringFromURLCompletedEventArgs e)
+        {
+            try
+            {
+                string data = e.Result.Substring(e.Result.IndexOf("<html>"));
+                page.Load(data);
+                if (LoadPageComplete != null)
+                    LoadPageComplete(this);
+            }
+            catch { }
+        }
+
+        private void root_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
