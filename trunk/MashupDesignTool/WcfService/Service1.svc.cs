11b2a3f15eab00c732fb46734397cd4d78076ced
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-10 08:39:54 +0000

    Tao project wcf service rieng ra.

---
+++ trunk/MashupDesignTool/WcfService/Service1.svc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Net;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        public string GetStringFromURL(string url)
+        {
+            WebClient webClient = new WebClient();
+            return webClient.DownloadString(url);
+        }
+
+        public byte[] GetDataFromURL(string url)
+        {
+            WebClient webClient = new WebClient();
+            return webClient.DownloadData(url);
+        }
+    }
+}
