053f4d703b985dd209377aecb51dfb8ef3c69793
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-13 11:03:04 +0000

    + add thu muc pixelshader + thêm property IsSelfHandle, hàm SetSelfHandle để cho biết hiệu ứng tự chạy hay là người dùng gọi, mặc định là tự chạy. + thêm các hiệu ứng mới

---
+++ MashupDesignTool/EffectLibrary/CustomPixelShader/Ultily.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Reflection;
+
+namespace EffectLibrary
+{
+    public class Ultily
+    {
+        public static Uri MakePackUri(string relativeFile)
+        {
+            string uriString = "/" + AssemblyShortName + ";component/CustomPixelShader/HLSL/" + relativeFile;
+            return new Uri(uriString, UriKind.Relative);
+        }
+
+        private static string AssemblyShortName
+        {
+            get
+            {
+                if (_assemblyShortName == null)
+                {
+                    Assembly a = typeof(Ultily).Assembly;
+                    // Pull out the short name.
+                    _assemblyShortName = a.ToString().Split(',')[0];
+                }
+                return _assemblyShortName;
+            }
+        }
+
+        private static string _assemblyShortName;
+    }
+}
---
+++ MashupDesignTool/EffectLibrary/Ripple.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.Generic;
+using BasicLibrary;
+
+namespace EffectLibrary
+{
+    public class Ripple : BasicEffect
+    {
+        int _speed;
+        double _progressFrom;
+        double _progressTo;
+
+        public double ProgressTo
+        {
+            get { return _progressTo; }
+            set 
+            { 
+                _progressTo = value;
+                ((EasingDoubleKeyFrame)((DoubleAnimationUsingKeyFrames)sbEnter.Children[0]).KeyFrames[1]).Value = _progressTo;
+            }
+        }
+        public double ProgressFrom
+        {
+            get { return _progressFrom; }
+            set 
+            { 
+                _progressFrom = value;
+                ((EasingDoubleKeyFrame)((DoubleAnimationUsingKeyFrames)sbEnter.Children[0]).KeyFrames[0]).Value = _progressFrom;
+            }
+        }
+        
+        public int Speed
+        {
+            get { return _speed; }
+            set
+            {
+                _speed = value;
+                ((EasingDoubleKeyFrame)((DoubleAnimationUsingKeyFrames)sbEnter.Children[0]).KeyFrames[1]).KeyTime = TimeSpan.FromMilliseconds(_speed);                
+            }
+        }
+        
+        
+        public override void Start()
+        {
+            sbEnter.Begin();
+        }
+
+        public override void Stop()
+        {
+            
+        }
+
+        public override void DetachEffect()
+        {
+            control.Effect = null;
+            IsSelfHande = false;
+        }
+
+        public override void SetSelfHandle()
+        {
+            if (_isSelfHande == true)
+            {
+                control.MouseLeftButtonDown += new MouseButtonEventHandler(control_MouseLeftButtonDown);
+            }
+            else
+            {
+                control.MouseLeftButtonDown -= new MouseButtonEventHandler(control_MouseLeftButtonDown);
+            }
+        }
+        Storyboard sbEnter;
+       
+        public Ripple(EffectableControl control)
+            : base(control)
+        {
+            parameterNameList.Add("Speed");
+            parameterNameList.Add("ProgressFrom");
+            parameterNameList.Add("ProgressTo");
+            
+            sbEnter = new Storyboard();
+            
+            CustomPixelShader.RippleShader rs = control.Effect as CustomPixelShader.RippleShader;
+            if (rs == null)
+            {
+                rs = new CustomPixelShader.RippleShader();
+                control.Effect = rs;
+            }
+            _speed = 500;
+            _progressFrom = 0;
+            _progressTo = 1;
+            sbEnter.Children.Add(CreateDoubleAnimationUsingKeyFrames(rs, "Progress", _progressFrom, _progressTo, _speed));                                         
+        }
+
+        void control_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            sbEnter.Begin();
+        }
+
+        private static DoubleAnimationUsingKeyFrames CreateDoubleAnimationUsingKeyFrames(
+            DependencyObject element,
+            string property,
+            double from,
+            double to,
+            double milisecond)
+        {
+            DoubleAnimationUsingKeyFrames dakf = new DoubleAnimationUsingKeyFrames();
+            EasingDoubleKeyFrame edkf;
+
+            edkf = new EasingDoubleKeyFrame();
+            edkf.Value = from;
+            edkf.KeyTime = TimeSpan.FromMilliseconds(0);
+
+            dakf.KeyFrames.Add(edkf);
+
+            edkf = new EasingDoubleKeyFrame();
+            edkf.Value = to;
+            edkf.KeyTime = TimeSpan.FromMilliseconds(milisecond);
+
+            dakf.KeyFrames.Add(edkf);
+
+            Storyboard.SetTargetProperty(dakf, new PropertyPath(property));
+            Storyboard.SetTarget(dakf, element);
+
+            return dakf;
+        }
+    }
+}
