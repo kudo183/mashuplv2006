93a6ce19ec25266fbf4e41ecc823263ac6d8bf66
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-16 16:00:12 +0000

    + control ảnh gif động.

---
+++ CustomUserControl/AnimatedGifImage/AnimatedGifImage/AnimatedGifImage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace ControlLibrary
+{
+    public class AnimatedGifImage : ImageTools.Controls.AnimatedImage
+    {
+        public string ImageURL
+        {
+            get { return img.UriSource.OriginalString; }
+            set 
+            {
+                img = new ImageTools.Image();
+                img.UriSource = new Uri(value, UriKind.RelativeOrAbsolute);
+
+                img.LoadingCompleted += new EventHandler(img_LoadingCompleted);
+                img.LoadingFailed += new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
+                Source = img;                
+            }
+        }
+
+        ImageTools.Image img;
+        public AnimatedGifImage()
+        {
+            ImageTools.IO.Decoders.AddDecoder<ImageTools.IO.Gif.GifDecoder>();
+        }
+
+        void img_LoadingFailed(object sender, UnhandledExceptionEventArgs e)
+        {
+            img.LoadingCompleted -= new EventHandler(img_LoadingCompleted);
+            img.LoadingFailed -= new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
+        }
+
+        void img_LoadingCompleted(object sender, EventArgs e)
+        {
+            Dispatcher.BeginInvoke(UpdateLayout);
+            img.LoadingCompleted -= new EventHandler(img_LoadingCompleted);
+            img.LoadingFailed -= new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
+        }
+    }
+}
