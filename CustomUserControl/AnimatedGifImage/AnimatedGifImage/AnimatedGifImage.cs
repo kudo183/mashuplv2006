10d96fb10677d56fbfd090dd72cb53552a929676
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-17 07:20:36 +0000

    - fixbug crossdomain. + hỗ trợ bmp, jpg, png.

--- CustomUserControl/AnimatedGifImage/AnimatedGifImage/AnimatedGifImage.cs
+++ CustomUserControl/AnimatedGifImage/AnimatedGifImage/AnimatedGifImage.cs
@@ -8,42 +8,101 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using BasicLibrary;
+using System.ServiceModel;
+using System.IO;
+using System.Windows.Media.Imaging;
 
 namespace ControlLibrary
 {
-    public class AnimatedGifImage : ImageTools.Controls.AnimatedImage
+    public class AnimatedGifImage : BasicControl
     {
+        private string _ImageURL;
         public string ImageURL
         {
-            get { return img.UriSource.OriginalString; }
-            set 
+            get { return _ImageURL; }
+            set
             {
-                img = new ImageTools.Image();
-                img.UriSource = new Uri(value, UriKind.RelativeOrAbsolute);
+                string ext = value.Substring(value.LastIndexOf('.') + 1);
+                if(ext != "gif" && ext != "bmp" && ext != "jpg" && ext != "png")
+                    return;
+                _ImageURL = value;
 
-                img.LoadingCompleted += new EventHandler(img_LoadingCompleted);
-                img.LoadingFailed += new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
-                Source = img;                
+                client.GetDataFromURLAsync(_ImageURL);
             }
         }
 
-        ImageTools.Image img;
-        public AnimatedGifImage()
+        private bool _IsAnimated;
+        public bool IsAnimated
         {
-            ImageTools.IO.Decoders.AddDecoder<ImageTools.IO.Gif.GifDecoder>();
+            get { return _IsAnimated; }
         }
 
-        void img_LoadingFailed(object sender, UnhandledExceptionEventArgs e)
+        public int AnimationSpeed
         {
-            img.LoadingCompleted -= new EventHandler(img_LoadingCompleted);
-            img.LoadingFailed -= new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
+            get
+            {
+                if (_IsAnimated == true)
+                    return img.AnimationSpeed;
+                return 0;
+            }
+            set
+            {
+                if (_IsAnimated == true)
+                    img.AnimationSpeed = value;
+            }
+        }
+
+        void client_GetDataFromURLCompleted(object sender, ServiceReference1.GetDataFromURLCompletedEventArgs e)
+        {
+            try
+            {
+                BitmapImage bm = new BitmapImage();
+                bm.SetSource(new MemoryStream(e.Result));
+                ai.Source = bm;
+                _IsAnimated = false;
+                img = null;
+            }
+            catch (Exception ex)
+            {
+                img = new ImageTools.Image();
+
+                img.SetSource(new MemoryStream(e.Result));
+                img.LoadingCompleted += new EventHandler(img_LoadingCompleted);
+            }
         }
 
         void img_LoadingCompleted(object sender, EventArgs e)
         {
-            Dispatcher.BeginInvoke(UpdateLayout);
-            img.LoadingCompleted -= new EventHandler(img_LoadingCompleted);
-            img.LoadingFailed -= new EventHandler<UnhandledExceptionEventArgs>(img_LoadingFailed);
+            _IsAnimated = img.IsAnimated;
+            Dispatcher.BeginInvoke(SetImageSource);
+        }
+        private void SetImageSource()
+        {
+            ai.Source = img;
+        }
+
+        ImageTools.Controls.AnimatedImage ai = new ImageTools.Controls.AnimatedImage();
+        ImageTools.Image img;
+        ServiceReference1.Service1Client client;
+
+        public AnimatedGifImage()
+        {
+            parameterNameList.Add("ImageURL");
+            parameterNameList.Add("AnimationSpeed");
+            parameterNameList.Add("IsAnimated");
+
+            Content = ai;
+            ai.Width = ai.Height = 100;
+            ImageTools.IO.Decoders.AddDecoder<ImageTools.IO.Gif.GifDecoder>();
+            ImageTools.IO.Decoders.AddDecoder<ImageTools.IO.Bmp.BmpDecoder>();
+
+            BasicHttpBinding basicHttpBinding = new BasicHttpBinding();
+            basicHttpBinding.MaxReceivedMessageSize = 1 << 22;
+            EndpointAddress endpointAddress = new EndpointAddress("http://localhost:1728/Service1.svc");
+
+            client = new ServiceReference1.Service1Client(basicHttpBinding, endpointAddress);
+            client.GetDataFromURLCompleted += new EventHandler<ServiceReference1.GetDataFromURLCompletedEventArgs>(client_GetDataFromURLCompleted);
         }
     }
 }
