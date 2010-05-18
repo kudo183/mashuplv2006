c751035e3867d4051504a22d988c93774f34bf5b
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-18 13:29:41 +0000

    - implement giao diện IBasic trong BasicControl.

--- trunk/MashupDesignTool/BasicLibrary/BasicControl.cs
+++ trunk/MashupDesignTool/BasicLibrary/BasicControl.cs
@@ -8,33 +8,61 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.Xml;
+using System.Text;
+using System.Collections.Generic;
 
 namespace BasicLibrary
 {
     public class BasicControl : UserControl, IBasic
     {
         #region IBasic Members
-        public virtual string GetParameterNames()
+        protected List<string> parameterNameList = new List<string>();
+        public string GetParameterNames()
         {
-            return "";
+            StringBuilder sb = new StringBuilder();
+            XmlWriter xw = XmlWriter.Create(sb);
+            xw.WriteStartDocument();
+            xw.WriteStartElement("ParameterNames");
+
+            foreach (string name in parameterNameList)
+            {
+                xw.WriteStartElement("ParameterName");
+                xw.WriteValue(name);
+                xw.WriteEndElement();
+            }
+
+            xw.WriteEndElement();
+            xw.WriteEndDocument();
+            xw.Flush();
+            xw.Close();
+            return sb.ToString();
         }
 
-        public virtual Type GetParameterType(string parameterName)
+        public Type GetParameterType(string parameterName)
         {
+            if (parameterNameList.Contains(parameterName))
+                return this.GetType().GetProperty(parameterName).PropertyType;
             return null;
         }
 
-        public virtual object GetParameterValue(string parameterName)
+        public object GetParameterValue(string parameterName)
         {
+            if (parameterNameList.Contains(parameterName))
+                return this.GetType().GetProperty(parameterName).GetValue(this, null);
             return null;
         }
 
-        public virtual bool SetParameterValue(string parameterName, object value)
+        public bool SetParameterValue(string parameterName, object value)
         {
+            if (parameterNameList.Contains(parameterName))
+            {
+                this.GetType().GetProperty(parameterName).SetValue(this, Convert.ChangeType(value, GetParameterType(parameterName), null), null);
+                return true;
+            }
             return false;
         }
         #endregion
-
         protected BasicEffect mainEffect;
         public BasicEffect MainEffect
         {
--- trunk/MashupDesignTool/BasicLibrary/BasicListControl.cs
+++ trunk/MashupDesignTool/BasicLibrary/BasicListControl.cs
@@ -17,54 +17,7 @@ namespace BasicLibrary
 {
     public class BasicListControl : BasicControl
     {
-        protected List<string> parameterNameList = new List<string>();
-        #region IBasic Members
-        public override string GetParameterNames()
-        {
-            StringBuilder sb = new StringBuilder();
-            XmlWriter xw = XmlWriter.Create(sb);
-            xw.WriteStartDocument();
-            xw.WriteStartElement("ParameterNames");
-
-            foreach (string name in parameterNameList)
-            {
-                xw.WriteStartElement("ParameterName");
-                xw.WriteValue(name);
-                xw.WriteEndElement();
-            }
-
-            xw.WriteEndElement();
-            xw.WriteEndDocument();
-            xw.Flush();
-            xw.Close();
-            return sb.ToString();
-        }
-
-        public override Type GetParameterType(string parameterName)
-        {
-            if (parameterNameList.Contains(parameterName))
-                return this.GetType().GetProperty(parameterName).PropertyType;
-            return null;
-        }
-
-        public override object GetParameterValue(string parameterName)
-        {
-            if (parameterNameList.Contains(parameterName))
-                return this.GetType().GetProperty(parameterName).GetValue(this, null);
-            return null;
-        }
-
-        public override bool SetParameterValue(string parameterName, object value)
-        {
-            if (parameterNameList.Contains(parameterName))
-            {
-                this.GetType().GetProperty(parameterName).SetValue(this, Convert.ChangeType(value, GetParameterType(parameterName), null), null);
-                return true;
-            }
-            return false;
-        }
-        #endregion
-
+       
         public delegate void ListChangeHandler(string action, int index1, EffectableControl control, int index2);
         public event ListChangeHandler OnListChange;
 
