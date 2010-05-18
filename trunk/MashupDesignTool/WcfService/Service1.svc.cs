778f05a2f42079e180be20a0c6920f33f84f159d
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-18 17:25:13 +0000

    - them ham de gui mail o wcf service. Muon gui duoc fai vao fai config them username va password. - them control contact form.

--- trunk/MashupDesignTool/WcfService/Service1.svc.cs
+++ trunk/MashupDesignTool/WcfService/Service1.svc.cs
@@ -23,5 +23,29 @@ namespace WcfService
             WebClient webClient = new WebClient();
             return webClient.DownloadData(url);
         }
+
+        public bool SendMail(string fromName, string fromAddress, string toAddresses, string subject, string body)
+        {
+            try
+            {
+                System.Net.Mail.MailMessage msg = new System.Net.Mail.MailMessage();
+                msg.From = new System.Net.Mail.MailAddress(fromName + "<" + fromAddress + ">");
+                msg.ReplyTo = new System.Net.Mail.MailAddress(fromName + "<" + fromAddress + ">");
+                string[] array = toAddresses.Split(',');
+                foreach (string toAddress in array)
+                    msg.To.Add(new System.Net.Mail.MailAddress(toAddress));
+                msg.Subject = subject;
+                msg.Body = body;
+                msg.IsBodyHtml = true;
+
+                System.Net.Mail.SmtpClient smtp = new System.Net.Mail.SmtpClient();
+                smtp.Send(msg);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }        
+        }
     }
 }
