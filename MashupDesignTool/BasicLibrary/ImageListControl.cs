da64d86c12bfad94f47feb3cb4a28d4275fa5972
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-05-23 17:22:45 +0000

    + thêm ImageListControl, chỉ hỗ trợ PNG, JPG.

---
+++ MashupDesignTool/BasicLibrary/ImageListControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text;
+using System.Xml;
+
+namespace BasicLibrary
+{
+    public class ImageListControl : BasicListControl
+    {
+        public ImageListControl()
+            : base()
+        {
+            int count = 5;
+            for (int i = 0; i < count; i++)
+                AddItem(new ImageListControlItems());
+        }
+    }
+}
