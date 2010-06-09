62d8416f84f2fd7f4296ae2627572814772f1754
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-09 16:59:52 +0000

    - fix may cai bug linh tinh khi dem vao design tool

--- CustomUserControl/ListDataListItemLibrary/ImageTitleDataListItemControl/ImageTitleDataListItem.xaml.cs
+++ CustomUserControl/ListDataListItemLibrary/ImageTitleDataListItemControl/ImageTitleDataListItem.xaml.cs
@@ -24,9 +24,9 @@ namespace ImageTitleDataListItemControl
 
             parameterNameList.Add("TitleColor");
             parameterNameList.Add("ImageBorderColor");
-            parameterNameList.Add("Background");
-            parameterNameList.Add("BorderBrush");
-            parameterNameList.Add("BorderThickness");
+            parameterNameList.Add("ControlBackground");
+            parameterNameList.Add("ControlBorderBrush");
+            parameterNameList.Add("ControlBorderThickness");
             parameterNameList.Add("ZoomImageWhenEnter");
 
             parameterCanBindingNameList.Add("ImageURL");
@@ -44,23 +44,23 @@ namespace ImageTitleDataListItemControl
 
         public Color ImageBorderColor
         {
-            get { return ((SolidColorBrush)borderImage.Background).Color; }
-            set { borderImage.Background = new SolidColorBrush(value); }
+            get { return ((SolidColorBrush)borderImage.BorderBrush).Color; }
+            set { borderImage.BorderBrush = new SolidColorBrush(value); }
         }
 
-        public new Color Background
+        public Color ControlBackground
         {
             get { return ((SolidColorBrush)LayoutRoot.Background).Color; }
             set { LayoutRoot.Background = new SolidColorBrush(value); }
         }
 
-        public new Color BorderBrush
+        public Color ControlBorderBrush
         {
             get { return ((SolidColorBrush)LayoutBorder.BorderBrush).Color; }
             set { LayoutBorder.BorderBrush = new SolidColorBrush(value); }
         }
 
-        public new Thickness BorderThickness
+        public Thickness ControlBorderThickness
         {
             get { return LayoutBorder.BorderThickness; }
             set { LayoutBorder.BorderThickness = value; }
