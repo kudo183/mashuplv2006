c1d084389e2bc2a089359a9d80bead50c7eb7930
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-14 05:05:11 +0000

    - thêm lớp Serialize

---
+++ trunk/MashupDesignTool/BasicLibrary/serializer/BasicControlSerializer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text;
+using System.Xml;
+using BasicLibrary;
+using System.Xml.Linq;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BasicLibrary
+{
+    public class BasicControlSerializer
+    {
+        public static string Serialize(BasicControl control)
+        {
+            Type type = control.GetType();
+            if (!typeof(BasicControl).IsAssignableFrom(type))
+                return "";
+
+            BasicControl basicControl = (BasicControl)control;
+            StringBuilder sb = new StringBuilder();
+            XmlWriter xm = XmlWriter.Create(sb, new XmlWriterSettings() { OmitXmlDeclaration = true });
+
+            xm.WriteStartElement(type.Name);
+            xm.WriteAttributeString("Type", type.AssemblyQualifiedName);
+            List<string> propertyList = basicControl.GetParameterNameList();
+            foreach (string propertyName in propertyList)
+            {
+                object value = basicControl.GetParameterValue(propertyName);
+                if (value != null)
+                    xm.WriteRaw(MyXmlSerializer.Serialize(value, propertyName));
+            }
+
+            xm.WriteEndElement();
+            xm.Flush();
+            xm.Close();
+            return sb.ToString();
+        }
+
+        public static BasicControl Deserialize(string xml)
+        {
+            XmlReader reader = XmlReader.Create(new StringReader(xml));
+            XDocument doc = XDocument.Load(reader);
+            XElement root = doc.Root;
+
+            return (BasicControl)MyXmlSerializer.Load(root);
+        }
+    }
+}
