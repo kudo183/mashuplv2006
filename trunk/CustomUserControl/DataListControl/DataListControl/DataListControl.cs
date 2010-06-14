cbe8175082fe72f7b044076b6cbbfec5c55fb1d4
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-14 06:09:23 +0000

    - project DataListControl.

---
+++ trunk/CustomUserControl/DataListControl/DataListControl/DataListControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using BasicLibrary;
+using EffectLibrary;
+
+namespace ControlLibrary
+{
+    public class DataListControl:BasicDataListControl
+    {
+        public DataListControl()
+            : base()
+        {
+            Width = Height = 100;
+            listEffect = new EffectLibrary.CoverFlow(this);
+        }
+    }
+}
