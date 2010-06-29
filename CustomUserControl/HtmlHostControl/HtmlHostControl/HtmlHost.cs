3b66f3fa1b129614753e92305e8b4e281af8a231
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-29 09:11:24 +0000

    - dùng iframe cho htmlstring. - cập nhật vị trị ở sự kiện layoutUpdated.

--- CustomUserControl/HtmlHostControl/HtmlHostControl/HtmlHost.cs
+++ CustomUserControl/HtmlHostControl/HtmlHostControl/HtmlHost.cs
@@ -12,7 +12,7 @@ using System.Windows.Browser;
 using System.ComponentModel;
 using System.Windows.Data;
 using BasicLibrary;
-
+using System.Text;
 namespace ControlLibrary
 {
     public class HtmlHost : BasicControl
@@ -61,12 +61,21 @@ namespace ControlLibrary
             {
                 _HTMLUrl = value;
                 _HTMLString = "";
+                //if (divIFrameHost == null)
+                //    return;
+                //if (iFrame != null)
+                //    divIFrameHost.RemoveChild(iFrame);
+                //iFrame = CreateIFrameFromURL();
+                //divIFrameHost.AppendChild(iFrame);
+
                 if (divIFrameHost == null)
                     return;
-                if (iFrame != null)
-                    divIFrameHost.RemoveChild(iFrame);
-                iFrame = CreateIFrameFromURL();
-                divIFrameHost.AppendChild(iFrame);
+                if (iFrame == null)
+                {
+                    iFrame = CreateIFrame();
+                    divIFrameHost.AppendChild(iFrame);
+                }
+                SetIFrameSrc(_HTMLUrl);
             }
         }
 
@@ -79,35 +88,40 @@ namespace ControlLibrary
             {
                 _HTMLString = value;
                 _HTMLUrl = "";
+
                 if (divIFrameHost == null)
                     return;
-                if (iFrame != null)
+                if (iFrame != null)                   
                     divIFrameHost.RemoveChild(iFrame);
-                iFrame = CreateIFrameFromString();
+                iFrame = CreateIFrame();
                 divIFrameHost.AppendChild(iFrame);
-            }
-        }
 
-        int _htmlZIndex = 1;
-        [Category("HtmlHost")]
-        public int HtmlZIndex
-        {
-            get { return _htmlZIndex; }
-            set
-            {
-                _htmlZIndex = value;
-                if (divIFrameHost != null)
-                    divIFrameHost.SetStyleAttribute("z-index", _htmlZIndex.ToString());
+                SetIFrameContent(_HTMLString);
             }
         }
 
         private HtmlElement divIFrameHost;
-        private int padding = 5;
+        private int padding = 10;
+        HtmlElement iFrame;
+        string str_iFrame_id = "";
+
         public HtmlHost()
         {
             Loaded += new RoutedEventHandler(HtmlHost_Loaded);
+            LayoutUpdated += new EventHandler(HtmlHost_LayoutUpdated);
             SizeChanged += new SizeChangedEventHandler(HtmlHost_SizeChanged);
             Width = Height = 100;
+            parameterNameList.Add("HTMLString");
+            parameterNameList.Add("HTMLUrl");
+        }
+
+        void HtmlHost_LayoutUpdated(object sender, EventArgs e)
+        {
+            GeneralTransform gt = TransformToVisual(Application.Current.RootVisual);
+            Point p = gt.Transform(new Point(0, 0));
+            
+            HtmlControlLeft = (int)p.X + padding - 1;
+            HtmlControlTop = (int)p.Y + padding - 1;
         }
 
         void HtmlHost_SizeChanged(object sender, SizeChangedEventArgs e)
@@ -122,101 +136,76 @@ namespace ControlLibrary
         void HtmlHost_Loaded(object sender, RoutedEventArgs e)
         {
             InitializeComponent();
-            RegisterForNotification("Parent", this, new PropertyChangedCallback(OnParentValueChanged));
-
-            RegisterForNotification("(Canvas.Left)", this, new PropertyChangedCallback(OnLeftValueChanged));
-            RegisterForNotification("(Canvas.Top)", this, new PropertyChangedCallback(OnTopValueChanged));
         }
 
         public void InitializeComponent()
         {
+            if (divIFrameHost != null)
+                return;
             divIFrameHost = HtmlPage.Document.CreateElement("div");
-            divIFrameHost.SetStyleAttribute("position", "absolute");
             divIFrameHost.SetAttribute("id", System.Guid.NewGuid().ToString());
+
+            divIFrameHost.SetStyleAttribute("position", "absolute");
             divIFrameHost.SetStyleAttribute("left", _htmlControlLeft.ToString() + "px");
             divIFrameHost.SetStyleAttribute("top", _htmlControlTop.ToString() + "px");
-            divIFrameHost.SetStyleAttribute("z-index", _htmlZIndex.ToString());
-
+            
             HtmlPage.Document.Body.AppendChild(divIFrameHost);
         }
-        HtmlElement iFrame;
-        private HtmlElement CreateIFrameFromURL()
+
+        private void SetIFrameSrc(string url)
+        {
+            StringBuilder script = new StringBuilder();
+            
+            script.Append("var iframe = document.getElementById(\"" + str_iFrame_id + "\");");            
+            script.Append("iframe.src = \"" + url + "\";");
+
+            HtmlPage.Window.Eval(script.ToString());
+        }
+
+        private void SetIFrameContent(string content)
+        {
+            StringBuilder script = new StringBuilder();
+            
+            script.Append("var doc = document.getElementById(\"" + str_iFrame_id + "\").contentDocument;");
+            
+            script.Append("doc.open();");
+            script.Append("doc.write(\"<div>" + content.Replace("\"", "\\\"") + "</div>\");");
+            script.Append("doc.close();");
+
+            HtmlPage.Window.Eval(script.ToString());
+            
+            //HtmlPage.Window.Eval("ChangeIFrameContent(\"" + str_iFrame_id + "\"" + "," + "\"" + content.Replace("\"", "\\\"") + "\"" + ")");
+        }
+        
+        private HtmlElement CreateIFrame()
         {
             HtmlElement element;
             element = HtmlPage.Document.CreateElement("IFRAME");
-            element.SetAttribute("src", _HTMLUrl);
+            element.SetAttribute("src", "");
+            element.Id = Guid.NewGuid().ToString();
+            str_iFrame_id = element.Id;
+
             element.SetStyleAttribute("height", "100%");
             element.SetStyleAttribute("width", "100%");
             element.SetStyleAttribute("left", "0px");
             element.SetStyleAttribute("position", "relative");
             element.SetStyleAttribute("top", "0px");
-            element.Id = System.Guid.NewGuid().ToString();
+
+            //element.Id = str_iFrame_id; tuong duong voi element.SetAttribute("ID", str_iFrame_id);
             return element;
         }
-
-        private HtmlElement CreateIFrameFromString()
-        {
-            HtmlElement element;
-            element = HtmlPage.Document.CreateElement("DIV");
-
-            if (HtmlPage.BrowserInformation.Name == "Netscape")
-                element.SetProperty("innerHTML", _HTMLString);
-            else
-                element.SetAttribute("innerHTML", _HTMLString);
-
-            element.SetStyleAttribute("overflow", "scroll");
-            element.SetStyleAttribute("height", "100%");
-            element.SetStyleAttribute("width", "100%");
-            element.SetStyleAttribute("left", "0px");
-            element.SetStyleAttribute("position", "relative");
-            element.SetStyleAttribute("top", "0px");
-            element.Id = System.Guid.NewGuid().ToString();
-            return element;
-        }
-
-        private void RegisterForNotification(string propertyName, FrameworkElement element, PropertyChangedCallback callback)
-        {
-            //Bind to a depedency property
-            Binding b = new Binding(propertyName) { Source = element };
-            var prop = System.Windows.DependencyProperty.RegisterAttached(
-                "ListenAttached" + propertyName,
-                typeof(object),
-                typeof(UserControl),
-                new System.Windows.PropertyMetadata(callback));
-            element.SetBinding(prop, b);
-        }
-
-        private void OnLeftValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
-        {
-            FrameworkElement parent = o as FrameworkElement;
-            parent = parent.Parent as FrameworkElement;
-            GeneralTransform gt = parent.TransformToVisual(Application.Current.RootVisual);
-            Point p = gt.Transform(new Point(0, 0));
-            int d = int.Parse(args.NewValue.ToString());
-            HtmlControlLeft = (int)p.X + d + padding;
-            //MessageBox.Show("Left " + parent.Name + " " + d + " " + p.X.ToString() + " " + HtmlControlLeft.ToString());
-        }
-
-        private void OnTopValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
-        {
-            FrameworkElement parent = o as FrameworkElement;
-            parent = parent.Parent as FrameworkElement;
-            GeneralTransform gt = parent.TransformToVisual(Application.Current.RootVisual);
-            Point p = gt.Transform(new Point(0, 0));
-            int d = int.Parse(args.NewValue.ToString());
-            HtmlControlTop = (int)p.Y + d + padding;
-            // MessageBox.Show("Top " + parent.Name + " " + d + " " + p.Y.ToString() + " " + HtmlControlTop.ToString());
-        }
-
-        private void OnParentValueChanged(DependencyObject o, DependencyPropertyChangedEventArgs args)
-        {
-            FrameworkElement parent = args.NewValue as FrameworkElement;
-            parent = parent.Parent as FrameworkElement;
-            //MessageBox.Show(parent.Name + " : " + parent.GetType().ToString());
-
-            RegisterForNotification("(Canvas.Left)", parent, new PropertyChangedCallback(OnLeftValueChanged));
-            RegisterForNotification("(Canvas.Top)", parent, new PropertyChangedCallback(OnTopValueChanged));
-        }
+        
+        //private void RegisterForNotification(string propertyName, FrameworkElement element, PropertyChangedCallback callback)
+        //{
+        //    //Bind to a depedency property
+        //    Binding b = new Binding(propertyName) { Source = element };
+        //    var prop = System.Windows.DependencyProperty.RegisterAttached(
+        //        "ListenAttached" + propertyName,
+        //        typeof(object),
+        //        typeof(UserControl),
+        //        new System.Windows.PropertyMetadata(callback));
+        //    element.SetBinding(prop, b);
+        //}
 
         public override void Dispose()
         {
