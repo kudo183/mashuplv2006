407d67bf2fedc520eb750c8dde251e294dab3c24
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-29 12:15:01 +0000

    - them file bi commit thieu

---
+++ trunk/MashupDesignTool/MashupDesignTool/Event/EventEditor.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using BasicLibrary;
+
+namespace MashupDesignTool
+{
+    public partial class EventEditor : ChildWindow
+    {
+        List<ControlComboBoxItemData> listControls;
+        MDTEventInfo mdtei;
+        BasicControl raiseControl;
+        List<ComboBox> cbbHandleControls = new List<ComboBox>();
+        List<ComboBox> cbbHandleOperations = new List<ComboBox>();
+        double rowHeight = 30;
+        MDTEventInfo newMdtei = null;
+
+        public EventEditor(BasicControl raiseControl, string eventName, List<ControlComboBoxItemData> listControls, MDTEventInfo mdtei)
+        {
+            InitializeComponent();
+
+            lblEventName.Content = eventName;
+            this.raiseControl = raiseControl;
+            this.listControls = listControls;
+            this.mdtei = mdtei;
+            if (mdtei == null)
+            {
+                AddRow();
+                rdNone.IsChecked = true;
+            }
+            else
+            {
+                LoadGridEventBinding();
+                rdHandle.IsChecked = true;
+            }
+        }
+
+        public bool IsHandled
+        {
+            get { return (newMdtei != null); }
+        }
+
+        public MDTEventInfo NewMdtei
+        {
+            get { return newMdtei; }
+        }
+
+        private void LoadGridEventBinding()
+        {
+            for (int i = 0; i < mdtei.HandleControls.Count; i++)
+                AddRow(mdtei.HandleControls[i], mdtei.HandleOperations[i]);
+        }
+
+        private void OKButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (rdHandle.IsChecked == true)
+            {
+                List<BasicControl> handleControls = new List<BasicControl>();
+                List<string> handleOperations = new List<string>();
+
+                for (int i = 0; i < cbbHandleOperations.Count; i++)
+                {
+                    if (cbbHandleOperations[i].Tag != null && cbbHandleOperations[i].SelectedIndex != -1)
+                    {
+                        handleControls.Add((BasicControl)cbbHandleOperations[i].Tag);
+                        handleOperations.Add((string)cbbHandleOperations[i].SelectedItem);
+                    }
+                }
+                newMdtei = MDTEventManager.RegisterEvent(raiseControl, (string)lblEventName.Content, handleControls, handleOperations);
+            }
+            else
+            {
+                if (mdtei != null)
+                    MDTEventManager.RemoveEvent(mdtei.RaiseControl, mdtei.EventName);
+                newMdtei = null;
+            }
+
+            this.DialogResult = true;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+        }
+
+        #region grid splitter
+        bool _isMouseDownOnGridTitleGridSplitter;
+        private void GridSplitter_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_isMouseDownOnGridTitleGridSplitter == false)
+                return;
+            gridEventBinding.ColumnDefinitions[0].Width = gridTitle.ColumnDefinitions[0].Width;
+            OKButton.Content = gridEventBinding.ColumnDefinitions[0].Width.ToString() + "  " + gridEventBinding.ColumnDefinitions[0].Width.ToString();
+        }
+
+        private void GridSplitter_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isMouseDownOnGridTitleGridSplitter = true;
+        }
+
+        private void GridSplitter_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            _isMouseDownOnGridTitleGridSplitter = false;
+        }
+        #endregion grid splitter
+
+        private void btnAddRow_Click(object sender, RoutedEventArgs e)
+        {
+            AddRow();
+        }
+
+        #region AddRow Function
+        private void AddRow()
+        {
+            AddRow(0, null, "");
+        }
+
+        private void AddRow(BasicControl handleControl, string handleOperation)
+        {
+            ControlComboBoxItemData item = null;
+            int index = 0;
+            for (int i = 0; i < listControls.Count; i++)
+                if (listControls[i].ControlName == handleControl.Name)
+                {
+                    index = i;
+                    item = listControls[i];
+                    break;
+                }
+            AddRow(index, item, handleOperation);
+        }
+
+        private void AddRow(int controlSelectedIndex, ControlComboBoxItemData controlSelectedItem, string handleOperation)
+        {
+            ComboBox control = new ComboBox();
+            control.Tag = cbbHandleControls.Count;
+            control.ItemTemplate = (DataTemplate)Resources["cbbControlDataTemplate"];
+            control.ItemsSource = listControls;
+            control.SelectedIndex = controlSelectedIndex;
+            control.Margin = new Thickness(0, 0, 0, 3);
+
+            ComboBox operation = new ComboBox();
+            operation.Margin = new Thickness(0, 0, 0, 3);
+            if (controlSelectedItem == null)
+            {
+                operation.IsEnabled = false;
+                operation.Tag = null;
+            }
+            else
+            {
+                List<string> list = controlSelectedItem.Control.GetListOperationName();
+                operation.Tag = controlSelectedItem.Control;
+                operation.IsEnabled = true;
+                operation.ItemsSource = list;
+                operation.SelectedItem = handleOperation;
+            }
+
+            control.SelectionChanged += new SelectionChangedEventHandler(cbbControl_SelectionChanged);
+
+            Border bd1 = new Border();
+            bd1.Child = control;
+            bd1.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 148, 173, 235));
+            bd1.BorderThickness = new Thickness(0, 0, 0, 1);
+            bd1.Margin = new Thickness(0, 3, 0, 0);
+
+            Border bd2 = new Border();
+            bd2.Child = operation;
+            bd2.BorderBrush = new SolidColorBrush(Color.FromArgb(255, 148, 173, 235));
+            bd2.BorderThickness = new Thickness(0, 0, 0, 1);
+            bd2.Margin = new Thickness(0, 3, 0, 0);
+            
+            gridEventBinding.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(rowHeight, GridUnitType.Pixel) });
+            //Grid.SetRow(control, cbbHandleControls.Count);
+            //Grid.SetRow(operation, cbbHandleControls.Count);
+            //Grid.SetColumn(operation, 1);
+            //gridEventBinding.Children.Add(control);
+            //gridEventBinding.Children.Add(operation);
+            Grid.SetRow(bd1, cbbHandleControls.Count);
+            Grid.SetRow(bd2, cbbHandleControls.Count);
+            Grid.SetColumn(bd2, 1);
+            gridEventBinding.Children.Add(bd1);
+            gridEventBinding.Children.Add(bd2);
+
+            cbbHandleControls.Add(control);
+            cbbHandleOperations.Add(operation);
+        }
+        #endregion AddRow Function
+
+        private void cbbControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ComboBox cbb = (ComboBox)sender;
+            ComboBox operation = cbbHandleOperations[(int)cbb.Tag];
+            if (cbb.SelectedIndex == 0)
+            {
+                operation.ItemsSource = new List<string>();
+                operation.IsEnabled = false;
+                operation.Tag = null;
+                return;
+            }
+            ControlComboBoxItemData item = (ControlComboBoxItemData)cbb.SelectedItem;
+            if (item == null)
+            {
+                operation.IsEnabled = false;
+                operation.Tag = null;
+                return;
+            }
+            List<string> list = item.Control.GetListOperationName();
+            operation.Tag = item.Control;
+            operation.IsEnabled = true;
+            operation.ItemsSource = list;
+        }
+    }
+}
+
---
+++ trunk/MashupDesignTool/MashupDesignTool/Serializer/PageSerializer.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Text;
+using System.Xml;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.IO;
+using BasicLibrary;
+
+namespace MashupDesignTool
+{
+    public class PageSerializer
+    {
+        public delegate void DeserializeCompletedHandler();
+        public event DeserializeCompletedHandler DeserializeCompleted; 
+        
+        private XElement canvasElement;
+        private DockCanvas.DockCanvas dockCanvas;
+        private DesignCanvas designCanvas;
+        private ControlDownloader controlDownloader;
+        private EffectDownloader effectDownloader;
+        private List<string> effectDll, effectReferenceDll;
+        private bool design;
+
+        #region Serialize
+        public static string Serialize(DesignCanvas designCanvas, List<string> controlDll, List<string> controlReferenceDll, List<string> effectDll, List<string> effectReferenceDll)
+        {
+            StringBuilder sb = new StringBuilder();
+            XmlWriter xm = XmlWriter.Create(sb, new XmlWriterSettings() { OmitXmlDeclaration = true });
+
+            xm.WriteStartElement("Page");
+            WriteNeccesaryDlls(xm, controlDll, controlReferenceDll, effectDll, effectReferenceDll);
+            xm.WriteRaw(DockCanvasSerializer.Serialize(designCanvas));
+            xm.WriteEndElement();
+
+            xm.Flush();
+            xm.Close();
+            return sb.ToString();
+        }
+
+        private static void WriteNeccesaryDlls(XmlWriter xm, List<string> controlDll, List<string> controlReferenceDll, List<string> effectDll, List<string> effectReferenceDll)
+        {
+            xm.WriteStartElement("NeccesaryDlls");
+            WriteDllList(xm, controlDll, "ControlDll");
+            WriteDllList(xm, controlReferenceDll, "ControlReferenceDll");
+            WriteDllList(xm, effectDll, "EffectDll");
+            WriteDllList(xm, effectReferenceDll, "EffectReferenceDll");
+            xm.WriteEndElement();
+        }
+
+        private static void WriteDllList(XmlWriter xm, List<string> dlls, string rootName)
+        {
+            xm.WriteStartElement(rootName);
+            foreach (string str in dlls)
+                xm.WriteElementString("Dll", str);
+            xm.WriteEndElement();
+        }
+        #endregion Serialize
+
+        #region Deserialize
+        public void Deserialize(string xml, DockCanvas.DockCanvas dockCanvas)
+        {
+            design = false;
+
+            List<string> controlDll = new List<string>();
+            List<string> controlReferenceDll = new List<string>();
+            List<string> effectDll = new List<string>();
+            List<string> effectReferenceDll = new List<string>();
+
+            XmlReader reader = XmlReader.Create(new StringReader(xml));
+            XDocument doc = XDocument.Load(reader);
+            XElement root = doc.Root;
+            XElement designCanvasElement = null;
+
+            foreach (XElement child in root.Elements())
+            {
+                if (child.Name == "DockCanvas")
+                    designCanvasElement = child;
+                else if (child.Name == "NeccesaryDlls")
+                {
+                    foreach (XElement childChild in child.Elements())
+                    {
+                        if (childChild.Name == "ControlDll")
+                            DeserializeListDlls(childChild, controlDll);
+                        else if (childChild.Name == "ControlReferenceDll")
+                            DeserializeListDlls(childChild, controlReferenceDll);
+                        else if (childChild.Name == "EffectDll")
+                            DeserializeListDlls(childChild, effectDll);
+                        else if (childChild.Name == "EffectReferenceDll")
+                            DeserializeListDlls(childChild, effectReferenceDll);
+                    }
+                }
+            }
+
+            this.controlDownloader = new ControlDownloader();
+            this.effectDownloader = new EffectDownloader();
+            this.canvasElement = designCanvasElement;
+            this.dockCanvas = dockCanvas;
+            this.effectDll = effectDll;
+            this.effectReferenceDll = effectReferenceDll;
+
+            controlDownloader.DownloadCompleted += new ControlDownloader.DownloadCompletedHandler(controlDownloader_DownloadCompleted);
+            controlDownloader.Download(controlDll, controlReferenceDll);
+        }
+
+        public void DeserializeInDesign(string xml, DesignCanvas designCanvas, ControlDownloader controlDownloader, EffectDownloader effectDownloader)
+        {
+            design = true;
+
+            List<string> controlDll = new List<string>();
+            List<string> controlReferenceDll = new List<string>();
+            List<string> effectDll = new List<string>();
+            List<string> effectReferenceDll = new List<string>();
+
+            XmlReader reader = XmlReader.Create(new StringReader(xml));
+            XDocument doc = XDocument.Load(reader);
+            XElement root = doc.Root;
+            XElement designCanvasElement = null;
+
+            foreach (XElement child in root.Elements())
+            {
+                if (child.Name == "DockCanvas")
+                    designCanvasElement = child;
+                else if (child.Name == "NeccesaryDlls")
+                {
+                    foreach (XElement childChild in child.Elements())
+                    {
+                        if (childChild.Name == "ControlDll")
+                            DeserializeListDlls(childChild, controlDll);
+                        else if (childChild.Name == "ControlReferenceDll")
+                            DeserializeListDlls(childChild, controlReferenceDll);
+                        else if (childChild.Name == "EffectDll")
+                            DeserializeListDlls(childChild, effectDll);
+                        else if (childChild.Name == "EffectReferenceDll")
+                            DeserializeListDlls(childChild, effectReferenceDll);
+                    }
+                }
+            }
+
+            this.controlDownloader = controlDownloader;
+            this.effectDownloader = effectDownloader;
+            this.canvasElement = designCanvasElement;
+            this.designCanvas = designCanvas;
+            this.effectDll = effectDll;
+            this.effectReferenceDll = effectReferenceDll;
+
+            controlDownloader.DownloadCompleted += new ControlDownloader.DownloadCompletedHandler(controlDownloader_DownloadCompleted);
+            controlDownloader.Download(controlDll, controlReferenceDll);
+        }
+
+        void controlDownloader_DownloadCompleted()
+        {
+            effectDownloader.DownloadCompleted += new EffectDownloader.DownloadCompletedHandler(effectDownloader_DownloadCompleted);
+            effectDownloader.Download(effectDll, effectReferenceDll);
+        }
+
+        void effectDownloader_DownloadCompleted()
+        {
+            DockCanvasSerializer dcs = new DockCanvasSerializer();
+            dcs.DeserializeCompleted += new DockCanvasSerializer.DeserializeCompletedHandler(dcs_DeserializeCompleted);
+            if (!design)
+                dcs.Deserialize(canvasElement, dockCanvas);
+            else
+                dcs.DeserializeInDesign(canvasElement, designCanvas);
+        }
+
+        void dcs_DeserializeCompleted()
+        {
+            if (DeserializeCompleted != null)
+                DeserializeCompleted();
+        }
+
+        private void DeserializeListDlls(XElement element, List<string> dlls)
+        {
+            foreach (XElement child in element.Elements())
+            {
+                if (child.Name == "Dll")
+                    dlls.Add(child.Value);
+            }
+        }
+        #endregion Deserialize
+    }
+}
