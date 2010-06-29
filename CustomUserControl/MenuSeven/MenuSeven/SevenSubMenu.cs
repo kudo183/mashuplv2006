962d36bcb821ffbdb2515a4127b0a26e8dcfd885
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-29 17:30:01 +0000

    - MenuSeven nửa vời :D

---
+++ CustomUserControl/MenuSeven/MenuSeven/SevenSubMenu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MyMenu
+{
+    public class SevenSubMenu : BasicLibrary.Menu.ISubMenu
+    {
+        Liquid.Menu _menu = new Liquid.Menu();
+
+        public Liquid.Menu Menu
+        {
+            get { return _menu; }
+            set { _menu = value; }
+        }
+        #region SubMenu Members
+        public void AddSubMenuItem(BasicLibrary.Menu.ISubMenuItem subMenuItem)
+        {
+            _menu.Items.Add((subMenuItem as SevenSubMenuItem).Item);
+        }
+
+        #endregion
+    }
+}
---
+++ CustomUserControl/MenuSeven/MenuSeven/SevenSubMenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace MyMenu
+{
+    public class SevenSubMenuItem : BasicLibrary.Menu.ISubMenuItem
+    {
+        Liquid.MenuItem _item = new Liquid.MenuItem();
+
+        public Liquid.MenuItem Item
+        {
+            get { return _item; }
+            set { _item = value; }
+        }
+        #region SubMenuItem Members
+        public void AddItemSubMenu(BasicLibrary.Menu.ISubMenu subMenu)
+        {
+            _item.Content = (subMenu as SevenSubMenu).Menu;
+        }
+
+        #endregion
+    }
+}
