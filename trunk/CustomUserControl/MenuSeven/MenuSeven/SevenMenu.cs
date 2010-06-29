9c83c592ada44dd086bb3261581cf060aadb04a8
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-06-29 17:30:01 +0000

    - MenuSeven nửa vời :D

---
+++ trunk/CustomUserControl/MenuSeven/MenuSeven/SevenMenu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Liquid;
+namespace MyMenu
+{
+    public class SevenMenu : BasicLibrary.Menu.BasicMenu
+    {
+        Grid LayoutRoot = new Grid() { Background = new SolidColorBrush(Colors.White) };
+        StackPanel spMenu;
+        Image img;
+        ImageSource _backgroundImage;
+
+        public ImageSource BackgroundImage
+        {
+            get { return _backgroundImage; }
+            set
+            {
+                _backgroundImage = value;
+                img.Source = _backgroundImage;
+            }
+        }
+
+
+        public SevenMenu()
+            : base()
+        {
+            parameterNameList.Add("BackgroundImage");
+            Content = LayoutRoot;
+            LayoutRoot.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            LayoutRoot.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+
+            img = new Image() { Stretch = Stretch.Fill };
+            LayoutRoot.Children.Add(img);
+
+            spMenu = new StackPanel() { Orientation = Orientation.Horizontal, Background = new SolidColorBrush(Colors.Transparent) };
+            spMenu.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+            spMenu.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
+            LayoutRoot.Children.Add(spMenu);
+        }
+
+        public override void ClearMenu()
+        {
+            spMenu.Children.Clear();
+        }
+        public override void AddMenuItem(BasicLibrary.Menu.IMenuItem item)
+        {
+            BasicLibrary.EffectableControl eff = new BasicLibrary.EffectableControl(item as SevenMenuItem);
+            eff.Width = 70;
+            eff.Height = 50;
+            eff.ChangeEffect("MainEffect", typeof(EffectLibrary.GlowWinSeven));
+            spMenu.Children.Add(eff);
+        }
+
+        public override BasicLibrary.Menu.IMenuItem CreateMenuItem(System.Xml.Linq.XElement e)
+        {
+            SevenMenuItem mmi = new SevenMenuItem(this);
+            System.Xml.Linq.XAttribute att;
+            att = e.Attribute("Text");
+            mmi.Title = att.Value;
+            att = e.Attribute("Icon");
+            mmi.ImageURL = att.Value;
+            return mmi;
+        }
+
+        public override BasicLibrary.Menu.ISubMenu CreateSubMenu()
+        {
+            SevenSubMenu msm = new SevenSubMenu();
+            return msm;
+        }
+
+        public override BasicLibrary.Menu.ISubMenuItem CreateSubMenuItem(System.Xml.Linq.XElement e)
+        {
+            SevenSubMenuItem msmi = new SevenSubMenuItem();
+            System.Xml.Linq.XAttribute att;
+            att = e.Attribute("Text");
+            msmi.Item.Text = att.Value;
+            att = e.Attribute("Icon");
+            msmi.Item.Icon = att.Value;
+            return msmi;
+        }
+    }
+}
---
+++ trunk/CustomUserControl/MenuSeven/MenuSeven/SevenMenuItem.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Media.Imaging;
+using System.Windows.Controls.Primitives;
+namespace MyMenu
+{
+    public class SevenMenuItem : BasicLibrary.BasicControl, BasicLibrary.Menu.IMenuItem
+    {
+        #region MenuItem Members
+
+        public void AddItemSubMenu(BasicLibrary.Menu.ISubMenu subMenu)
+        {
+            _subMenu = subMenu as SevenSubMenu;
+            _subMenu.Menu.ItemSelected += new Liquid.MenuEventHandler(Menu_ItemSelected);
+            _subMenu.Menu.MouseLeave += new MouseEventHandler(Menu_MouseLeave);
+            _subMenu.Menu.MouseEnter += new MouseEventHandler(Menu_MouseEnter);
+            p.Child = _subMenu.Menu;
+            Hide();
+        }
+
+        void Menu_ItemSelected(object sender, Liquid.MenuEventArgs e)
+        {
+            Hide();
+            _menu.OnSelectionChanged("");
+        }
+
+        #endregion
+
+        Image _Icon = new Image();
+        string _Title;
+
+        SevenSubMenu _subMenu;
+        Popup p = new Popup() { IsOpen = false };
+
+        private string _ImageURL;
+
+        public string ImageURL
+        {
+            get { return _ImageURL; }
+            set
+            {
+                _ImageURL = value;
+                _Icon.Source = new BitmapImage(new Uri(_ImageURL, UriKind.RelativeOrAbsolute));
+            }
+        }
+
+        public string Title
+        {
+            get { return _Title; }
+            set
+            {
+                _Title = value;
+                ToolTipService.SetToolTip(_Icon, _Title);
+            }
+        }
+
+        Grid LayoutRoot = new Grid();
+        SevenMenu _menu;
+
+        public SevenMenuItem(SevenMenu menu)
+        {
+            _menu = menu;
+            Content = LayoutRoot;
+            LayoutRoot.Children.Add(_Icon);
+            LayoutRoot.Children.Add(p);
+            _Icon.MouseEnter += new MouseEventHandler(_Icon_MouseEnter);
+            _Icon.MouseLeave += new MouseEventHandler(_Icon_MouseLeave);
+            ToolTipService.SetPlacement(_Icon, System.Windows.Controls.Primitives.PlacementMode.Top);
+        }
+
+        void Menu_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        void Menu_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Hide();
+        }
+        
+        void _Icon_MouseLeave(object sender, MouseEventArgs e)
+        {
+            Hide();
+        }
+        
+        void _Icon_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Show();
+        }
+
+        private void Hide()
+        {
+            if (_subMenu.Menu == null)
+                return;
+            _subMenu.Menu.Hide();
+            foreach (UIElement ui in _subMenu.Menu.Items)
+            {
+                Liquid.MenuItem mi = ui as Liquid.MenuItem;
+                if (mi != null)
+                {
+                    Liquid.Menu m = mi.Content as Liquid.Menu;
+                    if (m != null)
+                    {
+                        m.Hide();
+                    }
+                }
+            }
+            p.IsOpen = false;
+        }
+
+        private void Show()
+        {
+            if (_subMenu.Menu == null)
+                return;
+            _subMenu.Menu.Show();
+            p.VerticalOffset = _Icon.ActualHeight;
+            p.IsOpen = true;       }
+    }
+}
