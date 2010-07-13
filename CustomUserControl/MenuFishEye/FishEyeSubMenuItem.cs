1089678378e5ee1eae7f091f0c9a64a493e9a8d8
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-13 18:23:10 +0000

    - MenuFishEye, hạn dùng 30 ngày :D

---
+++ CustomUserControl/MenuFishEye/FishEyeSubMenuItem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using BasicLibrary;
+using ISNet.Silverlight.WebAqua;
+
+namespace MyMenu
+{
+    public class FishEyeSubMenuItem:WebFishEyeStackButton, BasicLibrary.Menu.ISubMenuItem
+    {
+        #region ISubMenuItem Members
+
+        public void AddItemSubMenu(BasicLibrary.Menu.ISubMenu subMenu)
+        {
+            
+        }
+
+        #endregion
+    }
+}
