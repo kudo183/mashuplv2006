26f68a9727800b744f9cc0fc00485eb0b1ad76d3
phamquochuy <phamquochuy@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-13 10:06:55 +0000

    - bổ sung các tùy chỉnh.

--- CustomUserControl/MenuSeven/MenuSeven/SevenMenu.cs
+++ CustomUserControl/MenuSeven/MenuSeven/SevenMenu.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Windows;
 using System.Windows.Controls;
@@ -8,55 +9,214 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.ComponentModel;
 using Liquid;
 namespace MyMenu
 {
     public class SevenMenu : BasicLibrary.Menu.BasicMenu
     {
-        Grid LayoutRoot = new Grid() { Background = new SolidColorBrush(Colors.White) };
+        Grid LayoutRoot = new Grid() { };
         StackPanel spMenu;
-        Image img;
-        ImageSource _backgroundImage;
 
-        public ImageSource BackgroundImage
+        #region menu property
+        Brush _Background;
+        [Category("Menu")]
+        public new Brush Background
         {
-            get { return _backgroundImage; }
+            get { return _Background; }
             set
             {
-                _backgroundImage = value;
-                img.Source = _backgroundImage;
+                _Background = value;
+                LayoutRoot.Background = Background;
             }
         }
 
+        byte _TransitionAlpha;
+        [Category("Menu")]
+        public byte TransitionAlpha
+        {
+            get { return _TransitionAlpha; }
+            set
+            {
+                _TransitionAlpha = value;
+                foreach (EffectLibrary.GlowWinSeven g in lstGS)
+                    g.TransitionAlpha = _TransitionAlpha;
+            }
+        }
 
+        Color _TransitionColor;
+        [Category("Menu")]
+        public Color TransitionColor
+        {
+            get { return _TransitionColor; }
+            set
+            {
+                _TransitionColor = value;
+                foreach (EffectLibrary.GlowWinSeven g in lstGS)
+                    g.TransitionColor = _TransitionColor;
+            }
+        }
+
+        double _ItemWidth;
+        [Category("Menu")]
+        public double ItemWidth
+        {
+            get { return _ItemWidth; }
+            set
+            {
+                _ItemWidth = value;
+                foreach (BasicLibrary.EffectableControl eff in lstEff)
+                    eff.Width = _ItemWidth;
+            }
+        }
+
+        double _ItemHeight;
+        [Category("Menu")]
+        public double ItemHeight
+        {
+            get { return _ItemHeight; }
+            set
+            {
+                _ItemHeight = value;
+                foreach (BasicLibrary.EffectableControl eff in lstEff)
+                    eff.Height = _ItemHeight;
+            }
+        }
+        #endregion
+
+        #region text block
+        FontFamily _Font;
+        [Category("MenuText")]
+        public FontFamily Font
+        {
+            get { return _Font; }
+            set
+            {
+                _Font = value;
+                tbTooptip.FontFamily = _Font;
+            }
+        }
+        double _TextSize;
+        [Category("MenuText")]
+        public double TextSize
+        {
+            get { return _TextSize; }
+            set
+            {
+                _TextSize = value;
+                tbTooptip.FontSize = _TextSize;
+            }
+        }
+
+        Brush _TextColor;
+        [Category("MenuText")]
+        public Brush TextColor
+        {
+            get { return _TextColor; }
+            set
+            {
+                _TextColor = value;
+                tbTooptip.Foreground = _TextColor;
+            }
+        }
+
+        Brush _TextBackground;
+        [Category("MenuText")]
+        public Brush TextBackground
+        {
+            get { return _TextBackground; }
+            set
+            {
+                _TextBackground = value;
+                tbTooptip.Foreground = _TextBackground;
+            }
+        }
+
+        double _TextAreaWidth;
+        [Category("MenuText")]
+        public double TextAreaWidth
+        {
+            get { return _TextAreaWidth; }
+            set
+            {
+                _TextAreaWidth = value;
+                tbTooptip.Width = _TextAreaWidth;
+            }
+        }
+        #endregion
         public SevenMenu()
             : base()
         {
-            parameterNameList.Add("BackgroundImage");
+            parameterNameList.Add("Background");
+            parameterNameList.Add("TransitionAlpha");
+            parameterNameList.Add("TransitionColor");
+            parameterNameList.Add("ItemWidth");
+            parameterNameList.Add("ItemHeight");
+
+            parameterNameList.Add("TextAreaWidth");
+            parameterNameList.Add("Font");
+            parameterNameList.Add("TextSize");
+            parameterNameList.Add("TextColor");
+            parameterNameList.Add("TextBackground");
+
+            _Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x9A, 0x09, 0x09));
+            _TransitionAlpha = 0x64;
+            _TransitionColor = Colors.Blue;
+            _ItemWidth = 70;
+            _ItemHeight = 50;
+
+            _Font = new System.Windows.Media.FontFamily("Portable User Interface");
+            _TextSize = 14;
+            _TextColor = new SolidColorBrush(Colors.White);
+            _TextAreaWidth = 120;
             Content = LayoutRoot;
             LayoutRoot.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
             LayoutRoot.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
-
-            img = new Image() { Stretch = Stretch.Fill };
-            LayoutRoot.Children.Add(img);
+            LayoutRoot.Background = Background;
 
             spMenu = new StackPanel() { Orientation = Orientation.Horizontal, Background = new SolidColorBrush(Colors.Transparent) };
             spMenu.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
             spMenu.VerticalAlignment = System.Windows.VerticalAlignment.Stretch;
             LayoutRoot.Children.Add(spMenu);
-        }
 
+            tbTooptip = new TextBlock()
+            {
+                Width = _TextAreaWidth,
+                VerticalAlignment = System.Windows.VerticalAlignment.Center,
+                TextAlignment = TextAlignment.Center,
+                IsHitTestVisible = false,
+                TextWrapping = TextWrapping.Wrap,
+                TextTrimming = TextTrimming.WordEllipsis,
+                FontSize = _TextSize,
+                Foreground = _TextColor
+            };
+            spMenu.Children.Add(tbTooptip);
+            tbTooptip.Text = "Tooltip";
+        }
+        TextBlock tbTooptip;
+
+        public TextBlock TbTooptip
+        {
+            get { return tbTooptip; }
+            private set { }
+        }
+        List<EffectLibrary.GlowWinSeven> lstGS = new List<EffectLibrary.GlowWinSeven>();
+        List<BasicLibrary.EffectableControl> lstEff = new List<BasicLibrary.EffectableControl>();
         public override void ClearMenu()
         {
             spMenu.Children.Clear();
+            spMenu.Children.Add(tbTooptip);
+            tbTooptip.Text = "Tooltip";
         }
         public override void AddMenuItem(BasicLibrary.Menu.IMenuItem item)
         {
             BasicLibrary.EffectableControl eff = new BasicLibrary.EffectableControl(item as SevenMenuItem);
-            eff.Width = 70;
-            eff.Height = 50;
-            eff.ChangeEffect("MainEffect", typeof(EffectLibrary.GlowWinSeven));
+            eff.Width = _ItemWidth;
+            eff.Height = _ItemHeight;
+            //eff.ChangeEffect("MainEffect", typeof(EffectLibrary.GlowWinSeven));
+            lstGS.Add(new EffectLibrary.GlowWinSeven(eff) { TransitionAlpha = _TransitionAlpha, TransitionColor = _TransitionColor });
             spMenu.Children.Add(eff);
+            lstEff.Add(eff);
         }
 
         public override BasicLibrary.Menu.IMenuItem CreateMenuItem(System.Xml.Linq.XElement e)
@@ -64,9 +224,11 @@ namespace MyMenu
             SevenMenuItem mmi = new SevenMenuItem(this);
             System.Xml.Linq.XAttribute att;
             att = e.Attribute("Text");
-            mmi.Title = att.Value;
+            if (att != null)
+                mmi.Title = att.Value;
             att = e.Attribute("Icon");
-            mmi.ImageURL = att.Value;
+            if (att != null)
+                mmi.ImageURL = att.Value;
             return mmi;
         }
 
@@ -81,9 +243,11 @@ namespace MyMenu
             SevenSubMenuItem msmi = new SevenSubMenuItem();
             System.Xml.Linq.XAttribute att;
             att = e.Attribute("Text");
-            msmi.Item.Text = att.Value;
+            if (att != null)
+                msmi.Item.Text = att.Value;
             att = e.Attribute("Icon");
-            msmi.Item.Icon = att.Value;
+            if (att != null)
+                msmi.Item.Icon = att.Value;
             return msmi;
         }
     }
