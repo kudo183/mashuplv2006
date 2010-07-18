60855c08eea7acbca711040c57b17e5ce81049df
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-18 17:06:24 +0000

    - them project TextBlock

---
+++ trunk/CustomUserControl/TextBlockControl/TextBlockControl/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace TextBlockControl
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            textBlock1.Text = "Helojdalkfj dslfja ldfl fdlja ljfjd asfja fljaflkja lfdkja d";
+            textBlock1.TextTrimming = TextTrimming.WordEllipsis;
+            textBlock1.TextColor = new SolidColorBrush(Colors.Red);
+            textBlock1.FontWeight = FontWeights.ExtraBold;
+            textBlock1.FontSize = 15;
+
+            strokeTextBlock1.Text = "Hello world";
+            strokeTextBlock1.StrokeOpacity = 0.8;
+            strokeTextBlock1.StrokeThickness = 8;
+            strokeTextBlock1.FontSize = 30;
+            strokeTextBlock1.Stroke = new SolidColorBrush(Colors.Red);
+            strokeTextBlock1.TextColor = new SolidColorBrush(Colors.Yellow);
+            strokeTextBlock1.ChangeText("<xml><Text>hello</Text></xml>");
+        }
+    }
+}
