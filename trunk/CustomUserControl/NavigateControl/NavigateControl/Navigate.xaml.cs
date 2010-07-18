205d8ddd7645961841e9685dfd0ca609af01ed8b
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-18 18:02:14 +0000

    - them project NavigateControl - sua cai StrokeTextBlock 1 chut

---
+++ trunk/CustomUserControl/NavigateControl/NavigateControl/Navigate.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using BasicLibrary;
+using System.Xml;
+using System.IO;
+using System.Windows.Browser;
+
+namespace NavigateControl
+{
+    public partial class Navigate : BasicControl
+    {
+        public Navigate()
+        {
+            InitializeComponent();
+
+            AddOperationNameToList("NavigateTo");
+        }
+
+        public void NavigateTo(string xml)
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create(new StringReader(xml));
+                while (reader.Read())
+                {
+                    if (reader.Name == "Link")
+                        HtmlPage.Window.Navigate(new Uri(reader.ReadInnerXml()));
+                }
+            }
+            catch { }
+        }
+
+        public void OpenNewWindow(string xml)
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create(new StringReader(xml));
+                while (reader.Read())
+                {
+                    if (reader.Name == "Link")
+                        HtmlPage.Window.Navigate(new Uri(reader.ReadInnerXml()), "_blank");
+                }
+            }
+            catch { }
+        }
+    }
+}
