c5cb9b6c3389de17f7a02fb0a3dcc610744037b3
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-19 04:39:39 +0000

    - them project HyperlinkControl

---
+++ trunk/CustomUserControl/SimpleControl/HyperlinkControl/WebLinkControl/WebLink.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using BasicLibrary;
+
+namespace WebLinkControl
+{
+    public partial class WebLink : BasicControl
+    {
+        public WebLink()
+        {
+            InitializeComponent();
+            parameterNameList.Add("Text");
+            parameterNameList.Add("TextColor");
+            parameterNameList.Add("FontStyle");
+            parameterNameList.Add("FontSize");
+            parameterNameList.Add("FontWeight");
+            parameterNameList.Add("TextBackground");
+            parameterNameList.Add("NavigateURL");
+            parameterNameList.Add("TextWrapping");
+
+            AddOperationNameToList("ChangeText");
+        }
+
+        public string Text
+        {
+            get { return webLink1.Text; }
+            set { webLink1.Text = value; }
+        }
+
+        public Brush TextColor
+        {
+            get { return webLink1.Foreground; }
+            set { webLink1.Foreground = value; }
+        }
+
+        public Brush TextBackground
+        {
+            get { return LayoutRoot.Background; }
+            set { LayoutRoot.Background = value; }
+        }
+
+        public FontStyle FontStyle
+        {
+            get { return webLink1.FontStyle; }
+            set { webLink1.FontStyle = value; }
+        }
+
+        public double FontSize
+        {
+            get { return webLink1.FontSize; }
+            set { webLink1.FontSize = Math.Max(0, Math.Min(100, value)); }
+        }
+
+        public FontWeight FontWeight
+        {
+            get { return webLink1.FontWeight; }
+            set { webLink1.FontWeight = value; }
+        }
+
+        public string NavigateURL
+        {
+            get { return webLink1.Uri; }
+            set { webLink1.Uri = value; }
+        }
+
+        public TextWrapping TextWrapping
+        {
+            get { return webLink1.TextWrapping; }
+            set { webLink1.TextWrapping = value; }
+        }
+    }
+}
