e111e8102bbe40bc98e4cefceba1db6ebe083c4e
tranphuonghai <tranphuonghai@4c234c1c-2bfd-11df-b580-1f2e1e9a714e>
2010-07-21 03:53:06 +0000

    - them project NewsDetailShower

---
+++ CustomUserControl/NewDetailShowerControl/test/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace test
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            newsDetailShower1.LoadData("<link>http://localhost:50431/55434/http:++vnexpress.net+GL+The-gioi+2010+07+3BA1E45B+</link>");
+        }
+    }
+}
